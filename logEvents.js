b63f1ccf554e228ea82d7247e8e814feab3648d7
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-10 09:22:06 +0100

    update

--- logEvents.js
+++ logEvents.js
@@ -2,12 +2,7 @@
 
 // to instal NPM packages, we need nmp -y
 
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
 
-const fs = require("fs");
-const fsPromises = require("fs").promises;
-const path = require("path");
 
 const logEvents = async (message, logName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
